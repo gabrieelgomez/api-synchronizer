3be66b44c62d2c5631b8c55ac686b8282b31300a
Gabriel Gomez <gagg1707@gmail.com>
2020-05-04 13:46:51 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# README
+
+Cimo API - Syncronizer
---
+++ app/synchronizers/cimo_synchronizer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'open-uri'
+
+# CimoSynchronizer Service for syncronizers stores
+class CimoSynchronizer
+  STAGING_URL = 'http://168.181.162.73/ords/pedidos_web2/vdatosiv'
+
+  def self.synchronize; end
+end
