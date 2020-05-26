f883e50918477d57cdb03cdd94426442f644b12d
Gabriel Gomez <gagg1707@gmail.com>
2020-05-25 21:23:34 -0400

    change dependencies capfile and rvm capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
